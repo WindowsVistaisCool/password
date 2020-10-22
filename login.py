1c1fc8069b5cb8e0a31864a7f18008d26140fdbc
WindowsVistaisCool <kwrush024@pccsk12.com>
2020-10-22 12:56:16 -0400

    added basictwostep

--- login.py
+++ login.py
@@ -17,3 +17,34 @@ def basic(password1, password2=None, password3=None):
 			print("Incorrect Password!")
 			invalid += 1
 		return False
+
+def basictwostep(main1=None, two1=None, main2=None, two2=None, main3=None, two3=None):
+	if main1 is None:
+		return 0
+	pw1 = [main1]
+	if two1 is None:
+		two1 = main1
+	pw2 = [two1]
+	if main2 is not None:
+		pw1.append(main2)
+		if two2 is None:
+			two2 = main2
+		pw2.append(two2)
+	if main3 is not None:
+		pw1.append(main3)
+		if two3 is None:
+			two3 = main3
+		pw2.append(two3)
+	entry = input("Please enter the password: ")
+	if entry in pw1:
+		print("Password Accepted.")
+		entry2 = input("Please enter the 2-auth password: ")
+		if entry2 in pw2:
+			print("Password Accepted")
+			return 2
+		else:
+			print("Wrong 2-auth password.")
+			return 1
+	else:
+		print("Wrong password")
+		return 0
--- test.py
+++ test.py
@@ -1,13 +1,33 @@
-from login import basic
+from login import basic, basictwostep
 
 class c:
 	w = '\033[91m'
 	g = '\033[92m'
+	y = '\033[93m'
+	b = '\033[94m'
 	e = '\033[0m'
 
-maint = basic("test")
+class grant:
+	def yes():
+		return f"{c.g}ACCESS GRANTED{c.e}"
+	def no():
+		return f"{c.w}ACCESS DENIED{c.e}"
+	def maybe():
+		return f"{c.y}ACCESS PENDING{c.e}"
 
-if maint == False:
-	print(f"{c.w}ACCESS DENIED{c.e}")
-elif maint == True:
-	print(f"{c.g}ACCESS GRANTED{c.e}")
+entry = input(f"{c.b}Please enter the number (1-2): {c.e}")
+if entry == "1":
+	maint = basic("test")
+
+	if maint == False:
+		print(grant.no())
+	elif maint == True:
+		print(grant.yes())
+elif entry == "2":
+	maint = basictwostep("test", "pass", "two", "step")
+	if maint == 0:
+		print(grant.no())
+	elif maint == 1:
+		print(grant.maybe())
+	elif maint == 2:
+		print(grant.yes())
